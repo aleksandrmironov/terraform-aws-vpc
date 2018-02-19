55ed351994dd12842d53fc1798635381efd72c42
Aleksandr Mironov <amironov@level5ops.com>
2018-02-19 19:00:38 +0300

    private natgw instead of public

--- main.tf
+++ main.tf
@@ -214,7 +214,7 @@ resource "aws_nat_gateway" "this" {
   count = "${var.create_vpc && var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.azs)) : 0}"
 
   allocation_id = "${element(local.nat_gateway_ips, (var.single_nat_gateway ? 0 : count.index))}"
-  subnet_id     = "${element(aws_subnet.public.*.id, (var.single_nat_gateway ? 0 : count.index))}"
+  subnet_id     = "${element(aws_subnet.private.*.id, (var.single_nat_gateway ? 0 : count.index))}"
 
   tags = "${merge(var.tags, map("Name", format("%s-%s", var.name, element(var.azs, (var.single_nat_gateway ? 0 : count.index)))))}"
 
